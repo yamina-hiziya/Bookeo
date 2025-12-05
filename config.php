ad413042e79ff0cb21aee5ab67027ab6b6e9806c
yamina <benhiziya7@gmail.com>
2025-12-05 11:07:15 +0100

    suppression du config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'db_name' => getenv('DB_NAME'),
-    'db_user' => getenv('DB_USER'),
-    'db_password' => getenv('DB_PASS'),
-    'db_port' => getenv('DB_PORT'),
-    'db_host' => getenv('DB_HOST'),
-];
